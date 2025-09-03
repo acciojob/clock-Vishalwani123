5a7607f9ce018d182d5d1ff58c0fd1d66fd863a4
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-03 18:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+
+    const formattedTime = now.toLocaleString('en-US', {
+        month: 'numeric',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+
+    timerElement.textContent = formattedTime;
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
